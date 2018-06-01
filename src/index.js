4e4c79ecba2b87f6230e93c5dc9d2a82c0b8f68b
BZ <bardichezanber@gmail.com>
2018-06-01 18:10:14 +0800

    fix

--- src/index.js
+++ src/index.js
@@ -194,7 +194,9 @@ export default class extends Component {
 
   componentWillReceiveProps (nextProps) {
     if (!nextProps.autoplay && this.autoplayTimer) clearTimeout(this.autoplayTimer)
-    this.setState(this.initState(nextProps, this.props.index !== nextProps.index))
+    if (this.props.index !== nextProps.index) {
+      this.setState(this.initState(nextProps, this.props.index !== nextProps.index))
+    }
   }
 
   componentDidMount () {
@@ -392,6 +394,9 @@ export default class extends Component {
    * @param  {string} dir    'x' || 'y'
    */
   updateIndex = (offset, dir, cb) => {
+    if (offset === undefined || this.internals.offset === undefined) {
+      return
+    }
     const state = this.state
     let index = state.index
     const diff = offset[dir] - this.internals.offset[dir]
